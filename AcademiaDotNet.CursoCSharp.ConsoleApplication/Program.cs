f71dd2b2d51842fe8722dc9f34851435c573eb52
Douglas Orsino <douglas.orsino123@outlook.com>
2020-07-03 21:41:44 -0300

    Estudando C# criei um sistema simples que printa os dias da semana utilizando Enum e condicionais

---
+++ AcademiaDotNet.CursoCSharp.ConsoleApplication/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcademiaDotNet.CursoCSharp.ConsoleApplication
+{
+    class Program
+    {
+        enum DiasUteisDaSemana
+        {
+            Sunday,
+            Monday, 
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday
+        };
+        static void Main(string[] args)
+        {
+            var diaDaSemana = DiasUteisDaSemana.Wednesday;
+
+            if(diaDaSemana == DiasUteisDaSemana.Monday)
+            {
+                Console.WriteLine("Estamos na Segunda Feira");
+
+            } else if(diaDaSemana == DiasUteisDaSemana.Tuesday)
+            {
+                Console.WriteLine("Estamos na Terça Feira");
+
+            } else if(diaDaSemana == DiasUteisDaSemana.Wednesday)
+            {
+                Console.WriteLine("Estamos na Quarta Feira");
+
+            } else if(diaDaSemana == DiasUteisDaSemana.Thursday)
+            {
+                Console.WriteLine("Estamos na Quinta Feira");
+
+            }else if(diaDaSemana == DiasUteisDaSemana.Friday)
+            {
+                Console.WriteLine("Estamos na Sexta Feira");
+
+            }else if(diaDaSemana == DiasUteisDaSemana.Saturday)
+            {
+                Console.WriteLine("Fim de semana - Sábado");
+
+            }else if(diaDaSemana == DiasUteisDaSemana.Sunday)
+            {
+                Console.WriteLine("Fim de semana - Domingo");
+            }
+            
+            Console.Read();
+        }
+    }
+}
